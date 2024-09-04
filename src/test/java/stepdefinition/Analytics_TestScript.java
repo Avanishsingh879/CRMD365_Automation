9848ae7aca92026b4f5ffdd33ca2cf55f8d4310c
Avanishsingh879 <96571635+Avanishsingh879@users.noreply.github.com>
2024-09-04 11:12:50 +0530

    File updated Date: 4-9-2024

--- src/test/java/stepdefinition/Analytics_TestScript.java
+++ src/test/java/stepdefinition/Analytics_TestScript.java
@@ -32,7 +32,7 @@ public class Analytics_TestScript {
 	
 	@Given("I navigate to APPlication open UrL")
 	public void i_navigate_to_APPlication_open_UrL() throws IOException, InterruptedException {
-	
+		
 		FileInputStream fis=new FileInputStream("Config.properties");
 		files=new Properties();
 		files.load(fis);
@@ -41,13 +41,60 @@ public class Analytics_TestScript {
 		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(files.getProperty("Url"));
-		System.out.println("Browser Launch done");
-		System.out.println("Login Test sucessfully12344");
-		Thread.sleep(2000);	 
+		System.out.println("Browser Launch");
+		Thread.sleep(2000);;
+	    
 	}
 
+	@When("User eneter username and password by click on Sign In Page")
+	public void user_eneter_username_and_password_by_click_on_Sign_In_Page() throws InterruptedException {
+		
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@name='user_name']")).sendKeys(files.getProperty("username"));
+		driver.findElement(By.xpath("//input[@name='user_password']")).sendKeys(files.getProperty("password"));
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@name='Login']")).click();
+		Thread.sleep(1000);
+	    
+	}
+
+	@Then("User verify Home Page Functionlity")
+	public void user_verify_Home_Page_Functionlity() {
+		
+		String ActualTitle=driver.getTitle();
+		String ExceptedTitle="admin - My Home Page - Home - vtiger CRM 5 - Commercial Open Source CRM";
+		if(ActualTitle.equalsIgnoreCase(ExceptedTitle)) {
+			
+		  System.out.println("Title Matched");
+		}
+		else {
+			
+			System.out.println("Title is not matched");
+		}
+	    
+	}
+
+	@Then("User able to click Analytics Tab")
+	public void user_able_to_click_Analytics_Tab() throws InterruptedException, IOException {
+		
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//table[@class='hdrTabBg']/tbody/tr/td[2]//a[text()='Analytics']")).click();
+		Thread.sleep(1000);
+		/////////////////////////////////////////////
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("./Screenshots/Analyiecs.png"));
+	    
+	}
 	
-	
+	@Then("User verify Report Page Functionlity")
+	public void user_verify_Report_Page_Functionlity() throws InterruptedException {
+	    
+		WebElement MouseHover=driver.findElement(By.xpath("//table[@class='hdrTabBg']/tbody/tr/td[2]//a[text()='Analytics']"));
+		Actions act=new Actions(driver);
+		act.moveToElement(MouseHover).build().perform();
+		driver.findElement(By.xpath("//div[@id='Analytics_sub']/table/tbody/tr/td//a[text()='Reports']")).click();
+		Thread.sleep(2000);
+	}
 
 }
 
--- src/test/resources/features/Analytics.feature
+++ src/test/resources/features/Analytics.feature
@@ -1,8 +1,20 @@
 Feature: Validating Analytics page
    
 
-@Regression   @Analytics_1  @TC_001
+@Regression   @Analytics_01  @TC_001
   Scenario: verify the Analytics Page
     Given I navigate to APPlication open UrL
+    When User eneter username and password by click on Sign In Page
+    Then User Login Sucessfully
+    And User verify Home Page Functionlity
+    And User able to click Analytics Tab
+    
+  @Regression   @Analytics_02  @TC_002  
+ Scenario: verify the Analyics Page
+    Given I navigate to Application open Url
+    When User eneter username and password by click on Sign In Page
+    Then User Login Sucessfully
+    And User verify Home Page Functionlity
+    And User verify Report Page Functionlity
     
      
