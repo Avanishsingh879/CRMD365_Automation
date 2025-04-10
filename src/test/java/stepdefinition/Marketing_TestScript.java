8d9b096545e84ecb11046d9fe0620b8ca193b38c
Avanishsingh879 <96571635+Avanishsingh879@users.noreply.github.com>
2025-04-10 09:57:54 +0530

    code updated date:10-4-2025

--- src/test/java/stepdefinition/Marketing_TestScript.java
+++ src/test/java/stepdefinition/Marketing_TestScript.java
@@ -36,18 +36,42 @@ public class Marketing_TestScript {
 		driver.get(files.getProperty("Url"));
 		Thread.sleep(2000);
 		System.out.println("Browser Launch");
+		/////////////////////////////////////////////////
+		/////////TakeScreenshots/////////////////////////
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("./Screenshots/Home.png"));
+		Thread.sleep(1000);
 	    
 		
 	}
 	
 
 	@When("User Enter UserName and Password by click on Sign In Button")
-	public void user_Enter_UserName_and_Password_by_click_on_Sign_In_Button() {
+	public void user_Enter_UserName_and_Password_by_click_on_Sign_In_Button() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//input[@name='user_name']")).sendKeys(files.getProperty("username"));
+		driver.findElement(By.xpath("//input[@name='user_password']")).sendKeys(files.getProperty("password"));
+		driver.findElement(By.xpath("//input[@name='Login']")).click();
+		Thread.sleep(1000);
+		
 	    
 	}
 
 	@Then("User Is in Home Page")
 	public void user_Is_in_Home_Page() {
+		
+		String ActualTitle=driver.getTitle();
+		String ExpTitle="";
+		
+		if(ActualTitle.equalsIgnoreCase(ExpTitle)) {
+			
+			System.out.println("Title Matched");
+		}
+		
+		else {
+			
+			System.out.println("Title is not Matched");
+		}
 	    
 	}
 	
