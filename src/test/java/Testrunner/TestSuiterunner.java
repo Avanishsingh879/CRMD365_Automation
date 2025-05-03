852af953ccc685af75cd32827158b445322e4707
Avanishsingh879 <96571635+Avanishsingh879@users.noreply.github.com>
2025-05-03 21:09:16 +0530

    Data 3-5-25

--- src/test/java/Testrunner/TestSuiterunner.java
+++ src/test/java/Testrunner/TestSuiterunner.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 		strict = true,
 		monochrome = true, 
 		features = {"src/test/resources/features/"},
-		tags={"@Marketing_3"},
+		tags={"@Marketing_2"},
 		glue = {"stepdefinition","Testrunner"}, 	
 		plugin = {
 	                "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
